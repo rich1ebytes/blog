1cd5309f3e1aa06cff674eba000b51c5b7c322fd
rich1ebytes <richardgomesxd@gmail.com>
2025-10-05 23:17:59 +0530

    title fixed

--- src/components/FavChannels.jsx
+++ src/components/FavChannels.jsx
@@ -3,6 +3,9 @@ export default function FavChannels() {
   return (
     <>
       <div>
+        <h1 className="subheading">
+          where all my free time goes
+        </h1>
          <ul className="para">
   {ytChannels.map((channel, index) => (
     <li key={index}>
