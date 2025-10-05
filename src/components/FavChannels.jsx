ed2d6701d981a0e467ab00ac691860adf0c738c1
rich1ebytes <richardgomesxd@gmail.com>
2025-10-05 23:05:46 +0530

    fixed channel urls

--- src/components/FavChannels.jsx
+++ src/components/FavChannels.jsx
@@ -3,16 +3,15 @@ export default function FavChannels() {
   return (
     <>
       <div>
-          <h1 className="subheading">where all my free time goes</h1>
-        <ul className="para">
-          {ytChannels.map((channel, index) => (
-            <li>
-              <a href="{channel.url}" key={channel}>
-                {channel.name}
-              </a>
-            </li>
-          ))}
-        </ul>
+         <ul className="para">
+  {ytChannels.map((channel, index) => (
+    <li key={index}>
+      <a href={channel.url} target="_blank" rel="noopener noreferrer">
+        {channel.name}
+      </a>
+    </li>
+  ))}
+</ul>
       </div>
     </>
   );
