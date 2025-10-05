e54014bf308c6c1e900020b254f405270b0c8d46
rich1ebytes <richardgomesxd@gmail.com>
2025-10-05 13:21:40 +0530

    made the home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,15 +1,51 @@
 function Home() {
   return (
     <>
-      <div className="flex flex-col gap-4 lg:gap-8">
+      <div className="flex flex-col gap-4 lg:gap-8 font-alan">
         <h1
           className="mt-10 text-5xl font-bold 
           sm:text-5xl md:text-5xl lg:text-6xl"
         >
           hi i'm richard!
         </h1>
-        <p className="text-xl text-zinc-600">
-          i love exploring and building stuff!
+        <p className="para">
+          someone who loves to explores and build things on the internet.
+        </p>
+        <p className="para">
+          i take interest in coding, making apps, learning new tech, and
+          running.
+        </p>
+        <p className="para">
+          you can{" "}
+          <a
+            href="https://portfoliorichard.vercel.app/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            find me here.
+          </a>
+        </p>
+        <p className="para">
+          apart from coding i enjoy researching about startups, economy and
+          politics.
+        </p>
+        <p className="para">
+          in the most of 2025 i did start multiple things but never completed.
+          be it a tech skill, a book to read, fitness, a new good habit or a
+          routine.
+        </p>
+        <p className="para">
+          but now, i think im a changed person, someone who looks forward in
+          life, stops complaining and does what it takes.
+        </p>
+        <p className="para">
+          currently im learning <a href="https://python.org" target="_blank">python</a> and <a href="https://en.wikipedia.org/wiki/SQL" target="_blank">sql</a> to work on data
+          thingyy, running daily and cooking (biryani). i find all these very
+          amusing and i think this time im gonna stick on to these.
+        </p>
+        <p className="para">
+          i don't regret trying multiple things out, its what made me come so
+          far. i'm gonna so many more cool stuff. <b>👹!</b>
         </p>
       </div>
     </>
